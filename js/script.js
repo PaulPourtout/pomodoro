69df032e9e1ac1a5ebfe3314e04324fe5439e914
PaulPourtout <paulpourtout@hotmail.com>
2017-02-12 23:39:15 +0100

    add rippleink effect on buttons

--- js/script.js
+++ js/script.js
@@ -10,48 +10,71 @@ var timeCountId = null;
 var clickCount = 0;
 
 var timeCount = function() {
-   if (secondesLeft === 0) {
-      minutesLeft--;
-      secondesLeft = 59;
-      minutes.textContent = minutesLeft;
-      secondes.textContent = secondesLeft;
-   }
-   else {
-      secondesLeft--;
-      if (secondesLeft < 10) {
-         secondes.textContent = "0" + secondesLeft;
-      }
-      else {
-         secondes.textContent = secondesLeft;
-      }
-   }
+    if (secondesLeft === 0) {
+        minutesLeft--;
+        secondesLeft = 59;
+        minutes.textContent = minutesLeft;
+        secondes.textContent = secondesLeft;
+    } else {
+        secondesLeft--;
+        if (secondesLeft < 10) {
+            secondes.textContent = "0" + secondesLeft;
+        } else {
+            secondes.textContent = secondesLeft;
+        }
+    }
 };
 
 
 startBtn.addEventListener("click", function() {
-   if (clickCount === 0) {
-      timeCountId = setInterval(timeCount, 1000);
-      startBtn.textContent = "Stop";
-      doublePoint.classList.add("blink");
-      clickCount = 1;
-   }
-   else {
-      clearInterval(timeCountId);
-      startBtn.textContent = "Start";
-      doublePoint.classList.remove("blink");
-      clickCount = 0;
-   }
+    if (clickCount === 0) {
+        timeCountId = setInterval(timeCount, 1000);
+        startBtn.textContent = "Stop";
+        doublePoint.classList.add("blink");
+        clickCount = 1;
+    } else {
+        clearInterval(timeCountId);
+        startBtn.textContent = "Start";
+        doublePoint.classList.remove("blink");
+        clickCount = 0;
+    }
 });
 
 var resetTimeCount = function() {
-   clearInterval(timeCountId);
-   startBtn.textContent = "Start";
-   clickCount = 0;
-   secondesLeft = 0;
-   minutesLeft = 25;
-   minutes.textContent = minutesLeft;
-   secondes.textContent = "0" + secondesLeft;
-   doublePoint.classList.remove("blink");
+    clearInterval(timeCountId);
+    startBtn.textContent = "Start";
+    clickCount = 0;
+    secondesLeft = 0;
+    minutesLeft = 25;
+    minutes.textContent = minutesLeft;
+    secondes.textContent = "0" + secondesLeft;
+    doublePoint.classList.remove("blink");
 };
 
 resetBtn.addEventListener("click", resetTimeCount);
+
+
+
+// RIPPLE EFFECT
+
+var buttons = document.querySelectorAll("#buttons-container button");
+
+buttons.forEach(function(item) {
+    item.addEventListener("click", function(e) {
+        var inkWidth = parseFloat(getComputedStyle(item).getPropertyValue("width"));
+        var inkHeight = parseFloat(getComputedStyle(item).getPropertyValue("height"));
+
+        x = e.pageX - item.offsetLeft - inkWidth / 2;
+        console.log("x", x);
+        y = e.pageY - item.offsetTop - inkWidth / 2;
+        console.log("y", y);
+        var ink = document.createElement("span");
+        ink.classList.add("ink");
+        ink.classList.add("animate");
+        ink.style.height = inkWidth + "px";
+        ink.style.width = inkWidth + "px";
+        ink.style.top = y + "px";
+        ink.style.left = x + "px";
+        item.appendChild(ink);
+    });
+});
